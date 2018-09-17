7de9088206dd44b03b4bc199bfb08b112b74e05c
Colin Bellino <bellino.colin@gmail.com>
2018-09-17 20:06:05 +0200

    ♻ Remove the mergeProps since it's not needed

--- src/components/PlayerHand/PlayerHand.container.ts
+++ src/components/PlayerHand/PlayerHand.container.ts
@@ -7,45 +7,34 @@ import {
   isCurrentPlayerId,
   selectHandTile,
 } from "../../store";
-import { EnhancedPlayerHand, EnhancedPlayerHandProps } from "./PlayerHand";
+import { EnhancedPlayerHand } from "./PlayerHand";
 
 type OwnProps = {
   playerId: Id;
 };
-type MergeProps = EnhancedPlayerHandProps & OwnProps;
 
-const mergeProps = (
-  stateProps,
-  dispatchProps,
-  ownProps: OwnProps,
-): MergeProps => {
-  const { state } = stateProps;
-  const { dispatch } = dispatchProps;
-  const { playerId } = ownProps;
-
-  const player = getPlayer(playerId)(state);
-  const isPlaying = isCurrentPlayerId(player.id)(state);
-
+const mapStateToProp = (state, ownProps: OwnProps) => {
   return {
-    ...ownProps,
-    player,
-    isPlaying,
-    onTileMouseEnter: ({ tileId }) => {
-      dispatch(focusHandTile(playerId, tileId));
-    },
-    onTileMouseLeave: ({ tileId }) => {
-      dispatch(focusHandTile(playerId, null));
-    },
-    onTileClick: ({ tileId }) => {
-      dispatch(selectHandTile(playerId, tileId));
-    },
+    player: getPlayer(ownProps.playerId)(state),
+    isPlaying: isCurrentPlayerId(ownProps.playerId)(state),
   };
 };
 
+const mapDispatchToProps = (dispatch, ownProps: OwnProps) => ({
+  onTileMouseEnter: ({ tileId }) => {
+    dispatch(focusHandTile(ownProps.playerId, tileId));
+  },
+  onTileMouseLeave: ({ tileId }) => {
+    dispatch(focusHandTile(ownProps.playerId, null));
+  },
+  onTileClick: ({ tileId }) => {
+    dispatch(selectHandTile(ownProps.playerId, tileId));
+  },
+});
+
 const enhance = connect(
-  state => ({ state }),
-  dispatch => ({ dispatch }),
-  mergeProps,
+  mapStateToProp,
+  mapDispatchToProps,
 );
 
 export default enhance(EnhancedPlayerHand);

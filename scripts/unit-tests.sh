a224103d87888799f3584e68b6376149a9ff6551
Colin Bellino <bellino.colin@gmail.com>
2018-09-29 18:51:03 +0200

    💚 Fix unit-tests script

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# Move to the actual game folder
+cd game/
+
 # Download the code climate test reporter
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
 chmod +x ./cc-test-reporter
 
 # Run the unit tests
-cd game/
 yarn install
 yarn test:unit
-cd ../
 
 # Upload the results to code climate
 ./cc-test-reporter format-coverage -t lcov -o ./coverage/coverage-code-climate.json ./coverage/lcov.info
